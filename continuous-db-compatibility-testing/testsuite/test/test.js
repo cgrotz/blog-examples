fecb3f56816a6f60dcc6e3dd4035b9a3c7a802c3
Christoph Grotz <grotz@google.com>
2022-08-21 14:00:58 +0200

    added test suite

--- continuous-db-compatibility-testing/testsuite/test/test.js
+++ continuous-db-compatibility-testing/testsuite/test/test.js
@@ -13,4 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ let chai = require('chai');
+ var expect = chai.expect
+ let chaiHttp = require('chai-http');
+ 
+ let should = chai.should();
+ chai.use(chaiHttp);
+ chai.use(require('chai-json'));
+ console.log("running tests against host", process.env.HOST)
+ describe('Users', function () {
+   describe('/GET users', () => {
+     it('it should GET all the users', (done) => {
+       chai.request(process.env.HOST)
+         .get('/users')
+         .end((err, res) => {
+           res.should.have.status(200);
+           res.header["content-type"].should.be.eql('application/json')
+           res.body.should.be.a('array');
+           res.body.length.should.be.gt(0);
+           done();
+         });
+     });
+   });
+ });
